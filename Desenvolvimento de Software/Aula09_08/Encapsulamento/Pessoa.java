4b1f102ae728200140b8309a7fac0201def1d778
Pedro Daroda <144063413+pedr0xh@users.noreply.github.com>
2024-08-09 08:43:24 -0300

    Update Pessoa.java

--- Desenvolvimento de Software/Aula09_08/Encapsulamento/Pessoa.java
+++ Desenvolvimento de Software/Aula09_08/Encapsulamento/Pessoa.java
@@ -8,5 +8,37 @@ public class Pessoa {
           
     private String nome;
     private int idade;
+
+    public Pessoa(String nome, int idade) {
+        setIdade(idade);
+        setNome(nome);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setNome(String nome) {
+        
+        if(!nome.isEmpty()){
+            this.nome = nome;
+        }else{
+            System.out.println("Nome em branco, digite algo!");
+        }
+    }
+
+    public void setIdade(int idade) {
+        if(idade>0){
+            this.idade = idade;
+        }else{
+            System.out.println("idade zerada, digite algo!");
+        }
+    }
+    
+    
     
 }
