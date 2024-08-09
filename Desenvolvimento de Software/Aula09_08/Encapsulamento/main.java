5303e437e3a37da95afeb6417adb4e2097b3f712
Pedro Daroda <144063413+pedr0xh@users.noreply.github.com>
2024-08-09 08:43:41 -0300

    Update main.java

--- Desenvolvimento de Software/Aula09_08/Encapsulamento/main.java
+++ Desenvolvimento de Software/Aula09_08/Encapsulamento/main.java
@@ -2,6 +2,8 @@ package aula;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Pessoa p = new Pessoa("Pedro",18);
+        System.out.println(p.getNome()+" "+p.getIdade());
+                
     }
 }
