e7b2323c444a614cfcfd22e9fd1b584d73d9612c
Pedro Daroda <144063413+pedr0xh@users.noreply.github.com>
2024-08-09 10:24:25 -0300

    Create Cachorro.java

---
+++ Desenvolvimento de Software/Aula09_08/Abstração/Cachorro.java
@@ -0,0 +1,14 @@
+package Abstracao;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Cachorro extends Animal{
+
+    @Override
+    public void fazSom() {
+        throw new UnsupportedOperationException("au au au");
+    }
+    
+}
