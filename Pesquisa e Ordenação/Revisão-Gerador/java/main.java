998961b6f6b24d1bbea6b5f14e2062fbe0f62069
Pedro Daroda <144063413+pedr0xh@users.noreply.github.com>
2024-08-14 10:41:22 -0300

    Update main.java

--- Pesquisa e Ordenação/Revisão-Gerador/java/main.java
+++ Pesquisa e Ordenação/Revisão-Gerador/java/main.java
@@ -1,27 +1,31 @@
-package Aula;
-
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
 import java.lang.Integer;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
     	
     	String nomeArquivo = "C:\\Users\\laboratorio\\Desktop\\aula\\palavra.txt";
     	Scanner ler = new Scanner(System.in);
     	ArrayList<String> listaPalavras = new ArrayList<>();
-        ArrayList<Integer> vetor = new ArrayList<>();
+        //ArrayList<Integer> vetor = new ArrayList<>();
 
         System.out.println("Quantas palavras quer gerar ? ");
         int quantidade = ler.nextInt();
         System.out.println("qual o tamanho das palavras ? ");
         int tamanho = ler.nextInt();
         
+       //popular(vetor, tamanho);
+        //exibir(vetor);
+        //Ordenacao.bolhaInteiros(vetor);
+        //exibir(vetor);
+        //System.out.println("Feitooooo...100");
         Util.gerarPalavrasArquivos(nomeArquivo,quantidade,tamanho);
-        //Util.gerarPalavraLista(listaPalavras,quantidade,tamanho);
-        //Util.exibir(listaPalavras,"LISTA DE PALAVRAS:");
-        
+        Util.gerarPalavraLista(listaPalavras,quantidade,tamanho);
+        Util.exibir(listaPalavras,"LISTA DE PALAVRAS:");
+        Ordenacao.bolhaPalavras(listaPalavras);
+        Util.exibir(listaPalavras, "LISTA DE PALAVRAS ORDENADAS");
         
     }
 
@@ -34,7 +38,8 @@ public class main {
 
     public static void exibir(ArrayList<Integer> v) {
         for (int i = 0; i < v.size(); i++) {
-            System.out.println(v.get(i));
+            System.out.print(v.get(i)+"\t");
         }
+        System.out.println("\n");
     }
 }
