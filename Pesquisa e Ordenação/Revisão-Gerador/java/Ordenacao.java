a41a573402bec398e22af6791456739abb52b220
Pedro Daroda <144063413+pedr0xh@users.noreply.github.com>
2024-08-14 21:50:52 -0300

    Update Ordenacao.java

--- Pesquisa e Ordenação/Revisão-Gerador/java/Ordenacao.java
+++ Pesquisa e Ordenação/Revisão-Gerador/java/Ordenacao.java
@@ -1,18 +1,17 @@
+package Testes;
+
 import java.util.ArrayList;
 
 public class Ordenacao {
-    
-    /**
-	 * Ordenação por metodo bolha para INTEIROS
-	 * @param lista = lista de inteiros que será ordenada
-	*/
+
+    // Método para ordenação por bolha de inteiros
     public static void bolhaInteiros(ArrayList<Integer> lista) {
         int aux;
         boolean troca;
-    
+
         for (int i = 0; i < lista.size() - 1; i++) {
             troca = false;
-    
+
             for (int j = 0; j < lista.size() - i - 1; j++) {
                 if (lista.get(j) > lista.get(j + 1)) {
                     troca = true;
@@ -21,35 +20,57 @@ public class Ordenacao {
                     lista.set(j + 1, aux);
                 }
             }
-    
+
             if (!troca) {
                 break;
             }
         }
     }
 
+    // Método para ordenação por bolha de strings
     public static void bolhaPalavras(ArrayList<String> lista) {
         String aux;
         boolean troca;
-    
+
         for (int i = 0; i < lista.size() - 1; i++) {
             troca = false;
-    
+
             for (int j = 0; j < lista.size() - i - 1; j++) {
                 if (lista.get(j).compareTo(lista.get(j + 1)) > 0) {
                     troca = true;
-                    // Troca dos elementos
                     aux = lista.get(j);
                     lista.set(j, lista.get(j + 1));
                     lista.set(j + 1, aux);
                 }
             }
-    
-            // Se nenhuma troca ocorreu, a lista já está ordenada
+
             if (!troca) {
                 break;
             }
         }
     }
-    
-}
+
+    // Método para ordenação por bolha de objetos do tipo Alunos
+    public static void bolhaObjeto(ArrayList<Alunos> lista) {
+        Alunos aux;
+        boolean troca;
+
+        for (int i = 0; i < lista.size() - 1; i++) {
+            troca = false;
+
+            for (int j = 0; j < lista.size() - i - 1; j++) {
+                // Corrigido: Remove o parêntese extra na comparação
+                if (lista.get(j).getNome().compareTo(lista.get(j + 1).getNome()) > 0) {
+                    troca = true;
+                    aux = lista.get(j);
+                    lista.set(j, lista.get(j + 1));
+                    lista.set(j + 1, aux);
+                }
+            }
+
+            if (!troca) {
+            }
+        }
+    }
+}       	
+            
