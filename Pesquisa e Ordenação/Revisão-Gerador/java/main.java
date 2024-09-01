92c84ceae16c0708602a3d593af60c8bd8dbdc2e
Pedro Henrique Daroda <pedro.daroda@ufn.edu.br>
2024-09-01 00:34:31 -0300

    Update main.java

--- Pesquisa e Ordenação/Revisão-Gerador/java/main.java
+++ Pesquisa e Ordenação/Revisão-Gerador/java/main.java
@@ -1,27 +1,55 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Random;
-import java.lang.Integer;
+import java.util.Collections;
 
-public class main {
+
+public class Main {
     public static void main(String[] args) {
-    	
-    	String nomeArquivo = "C:\\Users\\laboratorio\\Desktop\\aula\\palavra.txt";
-    	Scanner ler = new Scanner(System.in);
-    	ArrayList<String> listaPalavras = new ArrayList<>();
-        ArrayList<Integer> vetor = new ArrayList<>();
+
+        Scanner ler = new Scanner(System.in);
+
         ArrayList<Alunos> listaObj = new ArrayList<>();
 
         System.out.println("Quantos alunos deseja gerar ?");
         int quantidade = ler.nextInt();
-        
-        util.gerarAlunos(listaObj, quantidade);
-        util.exibirAlunos(listaObj);
-        System.out.println("================================================");
-        Ordenacao.bolhaObjeto(listaObj);
-        util.exibirAlunos(listaObj);
-        
-    }
 
-	
+        // Geração de alunos e adição à lista
+        Util.gerarAlunos(listaObj, quantidade);
+
+        // Ordenação dos objetos Alunos utilizando diferentes algoritmos em threads separadas
+
+        // Ordenação por bolha em uma thread
+        new Thread() {
+            public void run() {
+                Ordenacao.bolhaObjeto(listaObj);
+                System.out.println("Feitooooo.... bolha");
+            }
+        }.start();
+
+        // Ordenação por sort do Java em uma thread
+        new Thread() {
+            public void run() {
+                Collections.sort(listaObj);
+                System.out.println("Feitooooo.... sort java");
+            }
+        }.start();
+
+        // Ordenação por inserção em uma thread
+        new Thread() {
+            public void run() {
+                Ordenacao.insercaoObjeto(listaObj);
+                System.out.println("Feitooooo.... insercao");
+            }
+        }.start();
+
+        // Ordenação por seleção em uma thread
+        new Thread() {
+            public void run() {
+                Ordenacao.selecaoObjeto(listaObj);
+                System.out.println("Feitooooo.... selecao");
+            }
+        }.start();
+        
+
+    }
 }
