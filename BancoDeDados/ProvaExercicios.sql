725a466a08efb13104a9586171766e52dad1b4aa
Pedro Henrique Daroda <pedro.daroda@ufn.edu.br>
2024-09-30 09:24:55 -0300

    Update ProvaExercicios.sql

--- BancoDeDados/ProvaExercicios.sql
+++ BancoDeDados/ProvaExercicios.sql
@@ -13,11 +13,29 @@ JOIN PRE_REQUISITO AS P ON D.Numero_disciplina = P.Numero_disciplina
 JOIN DISCIPLINA AS D2 ON P.Numero_pre_requisito = D2.Numero_disciplina;
 
 --Questao 10
-SELECT A.Nome,D.Nome_disciplina
+SELECT A.Nome,D.Nome_disciplina,H.Nota
 FROM ALUNO AS A
 JOIN HISTORICO_ESCOLAR AS H ON A.Numero_aluno = H.Numero_aluno
 JOIN TURMA AS T ON H.Identificacao_turma = T.Identificacao_turma
 JOIN DISCIPLINA AS D ON T.Numero_disciplina = D.Numero_disciplina
 WHERE A.Nome = 'Silva';
+
+--Questao 11
+SELECT Nome, CONVERT(varchar,Data_Nascimento,103) AS DATA_NASCIMENTO
+FROM ALUNO;
+
+--Questao 12
+SELECT A.Nome, D.Nome_disciplina,
+    CASE H.Nota
+        WHEN 'A' THEN 10
+        WHEN 'B' THEN 9
+        WHEN 'C' THEN 8
+        WHEN 'F' THEN 0
+    END
+FROM ALUNO AS A
+JOIN HISTORICO_ESCOLAR AS H ON A.Numero_aluno = H.Numero_aluno
+JOIN TURMA AS T ON H.Identificacao_turma = T.Identificacao_turma
+JOIN DISCIPLINA AS D ON T.Numero_disciplina = D.Numero_disciplina
+
 
 
